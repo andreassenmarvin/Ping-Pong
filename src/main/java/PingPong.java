42dd188b184361d0d76e72fd46e638e471a9e1fa
andreassenmarvin <marvinandreassen@gmail.com>
2021-09-24 15:32:27 +0300

    Create test for numbers divisible by both 3 and 5

--- src/main/java/PingPong.java
+++ src/main/java/PingPong.java
@@ -5,7 +5,13 @@ public class PingPong {
     public ArrayList<Object> runPingPong(int countUpTo){
         ArrayList<Object> result = new ArrayList<Object>();
         for (int i = 1; i <= countUpTo; i++){
-            if(i % 3 == 0) {
+            if(i % 3 == 0 && i % 5 == 0) {
+                result.add("PingPong");
+            }
+            else if(i % 5 == 0) {
+                result.add("Pong");
+            }
+            else if(i % 3 == 0) {
                 result.add("Ping");
             }
             else {
--- src/test/java/PingPongTest.java
+++ src/test/java/PingPongTest.java
@@ -31,4 +31,38 @@ public class PingPongTest {
         expectedOutPut.add("Ping");
         assertEquals(expectedOutPut ,testPingPong.runPingPong(3));
     }
+
+    @Test
+    public void runPingPong_returnMultiplesOf5_ArrayList() {
+        PingPong testPingPong = new PingPong();
+        ArrayList<Object> expectedOutput = new ArrayList<Object>();
+        expectedOutput.add(1);
+        expectedOutput.add(2);
+        expectedOutput.add("Ping");
+        expectedOutput.add(4);
+        expectedOutput.add("Pong");
+        assertEquals(expectedOutput, testPingPong.runPingPong(5));
+    }
+
+    @Test
+    public void runPingPong_replaceMultiplesOfBoth3And5() {
+        PingPong testPingPong = new PingPong();
+        ArrayList<Object> expectedOutput = new ArrayList<Object>();
+        expectedOutput.add(1);
+        expectedOutput.add(2);
+        expectedOutput.add("Ping");
+        expectedOutput.add(4);
+        expectedOutput.add("Pong");
+        expectedOutput.add("Ping");
+        expectedOutput.add(7);
+        expectedOutput.add(8);
+        expectedOutput.add("Ping");
+        expectedOutput.add("Pong");
+        expectedOutput.add(11);
+        expectedOutput.add("Ping");
+        expectedOutput.add(13);
+        expectedOutput.add(14);
+        expectedOutput.add("PingPong");
+        assertEquals(expectedOutput ,testPingPong.runPingPong(15));
+    }
 }
