50efb3cfdedb41c9894fc031851146d3d824ca20
andreassenmarvin <marvinandreassen@gmail.com>
2021-09-24 16:36:11 +0300

    Add if else statements for users to select options

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,22 +1,35 @@
 import java.io.BufferedReader;
+import java.io.Console;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("I'm a ping-pong application!");
-        System.out.println("Enter a number:");
-        try {
-            String stringUserNumber = bufferedReader.readLine();
-            int intUserNumber = Integer.parseInt(stringUserNumber);
-            PingPong pingPong = new PingPong();
-            ArrayList<Object> pingPongResult = pingPong.runPingPong(intUserNumber);
-            System.out.println(pingPongResult);
-        }
-        catch (IOException e){
-            e.printStackTrace();
+        Console myConsole = System.console();
+        boolean programmeRunning = true;
+        while (programmeRunning) {
+            System.out.println("Welcome to ping-pong application!");
+            System.out.println("What would you like to do: proceed or exit");
+            String choice = myConsole.readLine();
+            if(choice.equals("proceed")) {
+                System.out.println("Enter a number");
+                try {
+                    String stringUserNumber = bufferedReader.readLine();
+                    int intUserNumber = Integer.parseInt(stringUserNumber);
+                    PingPong pingPong = new PingPong();
+                    ArrayList<Object> pingPongResult = pingPong.runPingPong(intUserNumber);
+                    System.out.println(pingPongResult);
+                }
+                catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+            else if (choice.equals("exit")) {
+                programmeRunning = false;
+            }
+
         }
     }
 }
--- src/main/java/PingPong.java
+++ src/main/java/PingPong.java
@@ -20,5 +20,4 @@ public class PingPong {
         }
         return result;
     }
-
 }
